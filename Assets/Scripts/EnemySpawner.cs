6d974141187febde6290696a4ab1bb56607528cd
aaronbrantley <aarongbrantley@gmail.com>
2022-10-17 15:41:11 -0700

    enemy attacks and follows player
    player doesnt have health

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     public int maxEnemies;
+    public int minSpawnDistance;
+    public int maxSpawnDistance;
     public GameObject enemy;
     public float spawnFrequency;
 
     private float spawnTimer;
+    private GameObject player;
 
     // Start is called before the first frame update
     void Start()
     {
         spawnTimer = spawnFrequency;
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
@@ -36,6 +40,15 @@ public class EnemySpawner : MonoBehaviour
     {
         GameObject newEnemy = Instantiate(enemy);
 
-        newEnemy.transform.parent = gameObject.transform;
+        newEnemy.transform.position = player.transform.position;
+
+        // spawn enemy closer to player
+        int distance = Random.Range(-maxSpawnDistance, maxSpawnDistance);
+        // dont let enemy spawn too close
+        while (Mathf.Abs (distance) < minSpawnDistance)
+        {
+            distance = Random.Range(-maxSpawnDistance, maxSpawnDistance);
+        }
+        newEnemy.transform.position += new Vector3 (distance, 0, 0);
     }
 }
