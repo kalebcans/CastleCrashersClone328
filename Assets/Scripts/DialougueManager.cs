eebacd78aa28a42dbfaaa206c351bf982813f5c7
cpezeshki <66799492+cpezeshki@users.noreply.github.com>
2022-10-20 13:08:19 -0700

    Added basic dialogue trigger

---
+++ Assets/Scripts/DialougueManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    private Queue<string> sentences;
+    public Text nameText;
+    public Text dialogueText;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();   
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        animator.SetBool("IsOpen", true);
+        //Debug.Log("Starting conversation with " + dialogue.name);
+        sentences.Clear();
+
+        nameText.text = dialogue.name;
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if(sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence (string sentence)
+    {
+        dialogueText.text = "";
+        foreach(char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+    public void EndDialogue()
+    {
+        animator.SetBool("IsOpen", false);
+    }
+
+    // Update is called once per frame
+
+}
