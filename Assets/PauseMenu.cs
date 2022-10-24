7e98a498f08a9c2770e853e94afcb1a5717c28a6
Mitchell Maddux <98301348+maddux-mitchell@users.noreply.github.com>
2022-10-24 10:33:53 -0700

    Main menu and pause menu

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool Paused = false;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(Paused){
+                Resume();
+            }
+            else{
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        Paused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        Paused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Debug.Log("Load Game");
+    }
+
+    public void QuitGame()
+    {
+        SceneManager.LoadScene("MainMenu");
+        Debug.Log("Quit Game");
+    }
+}
